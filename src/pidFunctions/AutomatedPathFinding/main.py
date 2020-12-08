ba0088658623425a4138c79b6f61c6c7ce2167b0
Sartaj Gill <1sartajgill@gmail.com>
2020-12-08 15:33:32 -0500

    Setup main.py

---
+++ src/pidFunctions/AutomatedPathFinding/main.py
@@ -0,0 +1,34 @@
+import numpy as num
+
+
+ceneredScored = False
+centerLeftScored = False
+centerRightScored = False
+
+FrontRightScored = False
+FrontCenterScored = False
+FrontLeftScored = False
+
+BackRight = False
+BackCenter = False
+BackLeft = False
+
+def IsFrontRightScored():
+    
+def IsFrontLeftScored():
+
+def IsFrontCenterScored():
+
+def IsBackLeftScored():
+
+def IsBackRightScored():
+
+def IsBackCenterScored():
+
+def IsCenterLeftScored():
+
+def IsCenterRightScored():
+
+while ():
+
+def main():
