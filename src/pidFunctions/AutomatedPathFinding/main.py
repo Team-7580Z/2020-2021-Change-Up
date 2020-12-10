18937350acf889b30fa8d2eea168e87580833a6f
Sartaj GIll <53071012+Sartaj21@users.noreply.github.com>
2020-12-10 00:28:51 -0500

    Will now caluclate total balls for us and find if we own the goal or not assuming we shot none of the other color back up

--- src/pidFunctions/AutomatedPathFinding/main.py
+++ src/pidFunctions/AutomatedPathFinding/main.py
@@ -1,24 +1,65 @@
 import numpy as np
 
 rows = 0
-def score(FrontLeftGoal, FrontRightGoal,FrontCenterGoal, CenterGoal, CenterRightGoal, CenterLeftGoal, BackRightGoal, BackLeftGoal, BackCenterGoal, OurBallsInFrontLeftGoal, OurBallsInFrontCenterGoal, OurBallsInFrontRightGoal, OurBallsInBackLeftGoal, OUrBallsInBackRightGoal, OUrBallsInBackCenterGoal, OurBallsinCenterRightGoal, OurBallsInCenterGoal, OurBallsIncenterLeftGoal, BallsDesocredinFrontLeft, BallsDescoredInFrontCenter, BallsDescoredInFrontRight, BallsDescoredInCenterLeft, BallsDescoredInCenter, BallsDescoredInCenterRight, BallsDescoredInBackLeft, BallsDescoredInBackCenter, BallsDescoredInBackRight):
+
+def score(OurBallsInFrontLeftGoal, OurBallsInFrontCenterGoal, OurBallsInFrontRightGoal, OurBallsInBackLeftGoal, OUrBallsInBackRightGoal, OUrBallsInBackCenterGoal, OurBallsinCenterRightGoal, OurBallsInCenterGoal, OurBallsIncenterLeftGoal, BallsDesocredinFrontLeft, BallsDescoredInFrontCenter, BallsDescoredInFrontRight, BallsDescoredInCenterLeft, BallsDescoredInCenter, BallsDescoredInCenterRight, BallsDescoredInBackLeft, BallsDescoredInBackCenter, BallsDescoredInBackRight):
+    rows = 0
+    if(OurBallsInFrontLeftGoal > 0):
+        FrontLeftGoal = True
+
+    if(OurBallsInFrontRightGoal > 0):
+        FrontRightGoal = True
+
+    if(OurBallsInFrontCenterGoal > 0):
+        FrontCenterGoal = True
+
+    if(OurBallsinCenterRightGoal > 0):
+        CenterRightGoal = True
+
+    if(OurBallsInCenterGoal > 0):
+        CenterGoal = True
+
+    if(OurBallsIncenterLeftGoal > 0):
+        CenterLeftGoal = True
+    
+    if(OurBallsInBackLeftGoal > 0):
+        BackLeftGoal = True
+    
+    if(OUrBallsInBackCenterGoal > 0):
+        BackCenterGoal = True
+
+    if(OUrBallsInBackRightGoal >0):
+        BackRightGoal = True
+
     if (FrontLeftGoal==True and FrontRightGoal==True and FrontCenterGoal==True):
         rows = rows + 1
+
     if (CenterGoal==True and CenterLeftGoal==True and CenterRightGoal==True):
         rows += 1
+
     if (BackCenterGoal==True and BackLeftGoal==True and BackRightGoal==True):
         rows += 1
+
     if (CenterGoal==True):
         if(FrontLeftGoal==True and BackRightGoal==True):
             rows += 1
+
         if(FrontRightGoal==True and BackLeftGoal==True):
             rows += 1
+
     if(FrontLeftGoal==True and CenterLeftGoal==True and BackLeftGoal==True):
         rows += 1
+
     if(FrontCenterGoal==True and CenterGoal==True and BackCenterGoal==True):
         rows += 1
+
     if(FrontRightGoal==True and CenterRightGoal==True and BackRightGoal==True):
         rows += 1
+
     else:
         rows = rows
-    
+
+
+    totalBalls = OUrBallsInBackCenterGoal+OurBallsInBackLeftGoal+OUrBallsInBackRightGoal+OurBallsInFrontCenterGoal+OurBallsInFrontRightGoal+OurBallsInFrontLeftGoal+OurBallsInCenterGoal+OurBallsIncenterLeftGoal+OurBallsIncenterLeftGoal
+
+    return totalBalls    
